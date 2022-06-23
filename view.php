6e764e14518ddc696d50332e0b79215cd4cd0c13
Allaseau <chenoa.299@gmail.com>
2022-06-23 19:29:04 +0200

    first push need correction

---
+++ view.php
@@ -0,0 +1,29 @@
+<?php
+getContactById($id) 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen Blanc</title>
+</head>
+<body>
+    <ul>
+    <?php foreach ($transactions as $t) :?>
+        <li>
+            <?= $t['date']?>
+            <?= 
+            if ($t['montant'] < 0 ) {
+                    echo strtoupper($t['destinataire'])
+                }else{
+                    echo strtoupper($t['emetteur'])
+                }
+            ?>
+            </a>
+        </li>
+    <?php endforeach ?>
+    </ul>
+</body>
+</html>
